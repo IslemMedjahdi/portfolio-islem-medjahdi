6e07e18c5bfe3a6d1cf179f703059b029aa157e2
IslemMedjahdi <ki_medjahdi@esi.dz>
2022-05-06 21:33:52 +0100

    Finishing projects Page

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,5 +1,56 @@
 import React from "react";
 
-export default function Project() {
-  return <div>Project</div>;
+type ProjectProps = {
+  id?: number;
+  name: string;
+  description: string;
+  github: string;
+  livePreview: string;
+  screenshot: string;
+  index: number;
+};
+
+export default function Project({
+  name,
+  description,
+  github,
+  livePreview,
+  screenshot,
+  index,
+}: ProjectProps) {
+  return (
+    <div className="reverse mt-6 grid items-center gap-10  md:grid-cols-2">
+      <div className="flex flex-col items-start">
+        <h2 className="font-Space text-2xl font-bold text-gray-700 dark:text-white">
+          {name}
+        </h2>
+        <p className="mt-6 font-Space  text-gray-800 dark:text-gray-50">
+          {description}
+        </p>
+        <div className="relative z-10 mt-10 transition active:scale-95">
+          <div className="absolute top-0 -z-10 h-full w-full translate-x-2 -translate-y-2 rounded-md border-2 border-blue-600 "></div>
+          <button
+            onClick={() => window.open(github, "_blank")}
+            className="rounded-md bg-blue-600 px-8  py-2 font-Space text-gray-50 transition duration-300 hover:bg-blue-500 active:scale-95"
+          >
+            View Project
+          </button>
+        </div>
+      </div>
+      <div
+        onClick={() => {
+          window.open(livePreview, "_blank");
+        }}
+        className={`group relative -order-1 cursor-pointer overflow-hidden  md:p-5 ${
+          index % 2 === 0 ? "md:order-none" : "md:-order-1"
+        } `}
+      >
+        <img
+          className="pointer-events-none h-full w-full rounded-2xl border-2 border-blue-500 object-cover shadow-lg transition duration-300 group-hover:scale-105"
+          src={screenshot}
+          alt="screenshot"
+        />
+      </div>
+    </div>
+  );
 }
--- src/screens/Projects.tsx
+++ src/screens/Projects.tsx
@@ -1,16 +1,62 @@
-import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Project from "../components/Project";
-
+import markdownScreenShot from "../assets/markdownScreenShot.png";
+import CountryFinderScreenShot from "../assets/CountryFinderScreenShot.png";
+import APSScreenShot from "../assets/APSScreenShot.png";
+import DOMEScreenShot from "../assets/DOMEScreenShot.png";
 type ProjectsProps = {
   theme: boolean;
   toggleTheme: () => void;
 };
 
-type Project = number;
+type ProjectType = {
+  id: number;
+  name: string;
+  description: string;
+  github: string;
+  livePreview: string;
+  screenshot: string;
+};
 
-const projects: Project[] = [1, 2, 3, 4];
+const projects: ProjectType[] = [
+  {
+    id: 1,
+    name: "Markdown Editor",
+    description:
+      "MarkDown Editor is a fullstack web application for creating, editing and saving markdowns with live preview.",
+    github: "https://github.com/IslemMedjahdi/markdown-editor",
+    livePreview: "https://markdown-editor-islemmedjahdi.vercel.app/",
+    screenshot: markdownScreenShot,
+  },
+  {
+    id: 2,
+    name: "Country Finder",
+    description:
+      "Country finder A website that allows you to browse the countries of the world with all the necessary information about the country with advanced search features",
+    github: "https://github.com/IslemMedjahdi/Country-finder",
+    livePreview: "https://country-finder-medjahdi.netlify.app/",
+    screenshot: CountryFinderScreenShot,
+  },
+  {
+    id: 3,
+    name: "APS: The art of problem solving",
+    description:
+      "APS: THE ART OF PROBLEM SOLVING is a website for the ESMS event of the CSE club.",
+    github: "https://github.com/IslemMedjahdi/ESMS12-APS-website",
+    livePreview: "https://aps-esms12.netlify.app/",
+    screenshot: APSScreenShot,
+  },
+  {
+    id: 4,
+    name: "DOME: todo app",
+    description:
+      "DOME is a TODO App that allows the user to add, delete and reorder the tasks of the todo list using drag and drop.",
+    github: "https://github.com/IslemMedjahdi/Do-Me-Todo-App",
+    livePreview: "https://dome-todoapp.netlify.app/",
+    screenshot: DOMEScreenShot,
+  },
+];
 export default function Projects({ theme, toggleTheme }: ProjectsProps) {
   return (
     <div className={`${theme ? "dark" : ""}`}>
@@ -26,8 +72,8 @@ export default function Projects({ theme, toggleTheme }: ProjectsProps) {
             Some things i've been working on in the past few years:
           </p>
           <div>
-            {projects.map((project) => (
-              <Project />
+            {projects.map((project, index) => (
+              <Project index={index} key={project.id} {...project} />
             ))}
           </div>
         </main>
